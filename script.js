5cd236dbf38794ecb5c10695fde08877a31b6b45
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-18 13:41:17 -0500

    practice

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let button = document.querySelector(".password-check");
 // Write a function to test if the password is correct
 // The function should take in the text of the password
 // as an input, and return true if the password is correct
+function passwordCheck(passwordToCheck){
+    if(passwordToCheck === "Mu$hu08"){
+        return true;
+    };
+        return false;
+};
 
 
 button.addEventListener("click", function() {
@@ -12,7 +18,10 @@ button.addEventListener("click", function() {
 
     // 2 - Call your function here, store the result in a variable
     // and use the variable to determine which message to display
-    if () {
+
+    let isPasswordCorrect = passwordCheck(password);
+
+    if (isPasswordCorrect) {
         document.querySelector(".yes").style = "display:block";
         document.querySelector(".denied").style = "display:none";
     } else {
